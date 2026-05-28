c8d4923018d94591ba317a1ac1e0981945f1b80f
Biswajit Kabi <biswajitkabi711@gmail.com>
2026-05-28 11:42:04 +0530

    fix: missing migrations

--- backend/apps/core/management/commands/createsu.py
+++ backend/apps/core/management/commands/createsu.py
@@ -5,20 +5,17 @@ import os
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         User = get_user_model()
+        username = os.getenv("DJANGO_SUPERUSER_USERNAME", "admin")
+        password = os.getenv("DJANGO_SUPERUSER_PASSWORD", "Admin@123")
 
-        username = os.getenv("DJANGO_SUPERUSER_USERNAME")
-        password = os.getenv("DJANGO_SUPERUSER_PASSWORD")
-
-        if not User.objects.filter(username=username).exists():
-            User.objects.create_superuser(
-                username=username,
-                password=password
-            )
-
-            self.stdout.write(
-                self.style.SUCCESS("Superuser created")
-            )
-        else:
-            self.stdout.write(
-                self.style.WARNING("Superuser already exists")
-            )
+        try:
+            if not User.objects.filter(username=username).exists():
+                User.objects.create_superuser(
+                    username=username,
+                    password=password
+                )
+                self.stdout.write(self.style.SUCCESS("Superuser created"))
+            else:
+                self.stdout.write(self.style.WARNING("Superuser already exists"))
+        except Exception as e:
+            self.stdout.write(self.style.ERROR(f"Error: {e}"))
